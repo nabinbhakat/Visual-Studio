64a4be98c16315a7c1e64924ce7ad438ec07985f
nabinbhakat <nabinbhakat9234@gmail.com>
2023-12-07 18:27:14 +0530

    07-12-2023

---
+++ java/Core/Collections/Arraylist.java
@@ -0,0 +1,15 @@
+/*
+ *  Arraylist is a dynamic array
+ *  working: Internally it uses a normal array with some default capacity.
+ *           When the capacity is reached it will create a new array of bigger
+ *           size(50% of its current size) and copies all the elements from the
+ *           old array to the new array.
+ *           it will reference to the new array 
+ *           the old array will be garabage collected in the next garbage collection 
+ */
+public class Arraylist {
+     public static void main(String[] args) {
+        
+    }
+    
+}
